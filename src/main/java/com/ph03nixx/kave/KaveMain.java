d0b4b3d9cbb555ded0c83e5d8c2bdcde0df78ef3
Gyula Krajczar <gyula.krajczar@gmail.com>
2015-08-20 09:27:53 +0200

    Initial commit of Kave

---
+++ src/main/java/com/ph03nixx/kave/KaveMain.java
@@ -0,0 +1,10 @@
+package com.ph03nixx.kave;
+
+public class KaveMain {
+
+	public static void main(String[] args) {
+		System.out.println("Kave - Java precompiler");
+		System.out.println("Copyright 2015 - Gyula Krajczar");
+	}
+
+}
